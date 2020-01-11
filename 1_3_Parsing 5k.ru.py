a1bb0417028c9263e1528fc2965377d27f114508
mmingalov <mmingalov@gmail.com>
2020-01-11 12:29:23 +1000

    done

--- 1_3_Parsing 5k.ru.py
+++ 1_3_Parsing 5k.ru.py
@@ -22,24 +22,56 @@ def get_data(url: str, params: dict) -> dict:
             break
     return response.json()
 
-#STARTING
-headers = {
-    'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko)',
-}
-params = {'records_per_page': 100, 'page': 1}
+def main_parser():
+    headers = {
+        'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko)',
+    }
+    params = {'records_per_page': 100, 'page': 1}
 
-url = 'https://5ka.ru/api/v2/special_offers/'
-results = []
+    url = 'https://5ka.ru/api/v2/special_offers/'
+    results = []
 
-while url:
-    response = get_data(url, params)
-    results.extend(response['results'])
-    url = response['next']
-    params = {}
-print(1)
+    while url:
+        response = get_data(url, params)
+        results.extend(response['results'])
+        url = response['next']
+        params = {}
+    print(1)
+    return results
 
-#ПРМЕЧАНИЕ: сайт не открываеся, ссылка 'https://5ka.ru/api/v2/special_offers/' возвращает пусто
-#включаем полную импровизацию. КОД будет отдаленно напоминать следующее:
 
-# for item in results: #у нас есть список товаров по акции
-#     dict = {'category_id': item['category_id'], category_name
+def save_data_json(name,results):
+    with open (name,'w') as f:
+        json_results=json.dump(results,f)
+
+def open_data_json():
+    with open ('1_3_special_offers.json','r') as f:
+        results = json.load(f)
+        return results
+
+#MAIN
+# results = main_parser()
+
+# save_data_json('1_3_special_offers.json',results) #ПРИМЕЧАНИЕ: сайт часто не открываеся, ссылка 'https://5ka.ru/api/v2/special_offers/' возвращает пусто. Поэтому для отладки сохраним результат в файл
+with open('1_3_special_offers.json', 'r') as f:
+    results = json.load(f)
+
+set_categ = set() #определили множество категорий товаров
+for d in results:
+    categ = d['promo']['description']
+    set_categ.add(categ)
+    # print(categ)
+
+list_categ = [] #определили список резульирующих словарей. Его размер совпадает с размером множества set_categ
+for s in set_categ:
+    dict = {'category_id':None, 'category_name':s, 'items':[]}
+    for d in results:
+        categ_name = d['promo']['description']
+        categ_id = d['promo']['id']
+        item_name = d['name']
+        if categ_name == s:
+            dict['category_id'] =categ_id
+            dict['items'].append(item_name)
+    list_categ.append(dict) #добавляем словарь со всеми товарами катгории s
+    save_data_json(s + '.json',dict)
+print(set_categ)

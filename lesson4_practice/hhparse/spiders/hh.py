298ac2c9129c02855d2bbb24c073ddd736388cf0
mmingalov <mmingalov@gmail.com>
2020-01-21 15:06:07 +1000

    done

--- lesson4_practice/hhparse/spiders/hh.py
+++ lesson4_practice/hhparse/spiders/hh.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-import re
 import scrapy
 from scrapy.http import HtmlResponse
+from scrapy.loader import ItemLoader
 from lesson4_practice.hhparse.items import hhparseItem
+
 
 # https://vladivostok.hh.ru/search/vacancy?L_is_autosearch=false&area=22&clusters=true&enable_snippets=true&text=%D0%BC%D0%B0%D1%80%D0%BA%D0%B5%D1%82%D0%BE%D0%BB%D0%BE%D0%B3&page=0
 class HhSpider(scrapy.Spider):
@@ -19,19 +20,26 @@ class HhSpider(scrapy.Spider):
             yield response.follow(url, callback=self.post_parse)
 
     def post_parse(self, response: HtmlResponse):
-        pattern = re.compile('[0123456789]')
+
         # item = hhparseItem(
-        url=response.url
-        title= response.xpath('//h1/span/text()').extract_first() # title=response.xpath('//h1[@class="header"]/span/text()').extract_first,
-
-        # salary=int(response.xpath('//div[@class="vacancy-title"]/p[@class="vacancy-salary"]/text()').extract()[1].replace('\xa0','')),
-        salary = ''.join(response.xpath('//div[@class="vacancy-title"]/p[@class="vacancy-salary"]/text()').extract()).replace('\xa0',''),
-
-        skills=response.xpath('//div[@class="vacancy-description"]//div[@class="vacancy-section"]//span[@data-qa="bloko-tag__text"]/text()').extract()
-        company_name=response.xpath('//div[@class="vacancy-company-wrapper"]/div[@data-qa="vacancy-company"]//a[@itemprop="hiringOrganization"]/span[@itemprop="name"]/span/text()').extract_first()
-        company_link=response.xpath('//div[@class="vacancy-company-wrapper"]/div[@data-qa="vacancy-company"]//a[@itemprop="hiringOrganization"]/@href').extract()
-        pass
+        #     url=response.url,
+        #     title= response.xpath('//h1/span/text()').extract_first(), # title=response.xpath('//h1[@class="header"]/span/text()').extract_first,
+        #     salary = ''.join(response.xpath('//div[@class="vacancy-title"]/p[@class="vacancy-salary"]/text()').extract()).replace('\xa0',''),
+        #
+        #     skills=response.xpath('//div[@class="vacancy-description"]//div[@class="vacancy-section"]//span[@data-qa="bloko-tag__text"]/text()').extract(),
+        #     company_name=response.xpath('//div[@class="vacancy-company-wrapper"]/div[@data-qa="vacancy-company"]//a[@itemprop="hiringOrganization"]/span[@itemprop="name"]/span/text()').extract_first(),
+        #     company_link=response.xpath('//div[@class="vacancy-company-wrapper"]/div[@data-qa="vacancy-company"]//a[@itemprop="hiringOrganization"]/@href').extract(),
         # )
+
+        item = ItemLoader(hhparseItem(),response)
+        item.add_value('url', response.url)
+        item.add_xpath('title', '//h1/span/text()')
+        item.add_xpath('salary','//div[@class="vacancy-title"]/p[@class="vacancy-salary"]/text()')
+        item.add_xpath('skills','//div[@class="vacancy-description"]//div[@class="vacancy-section"]//span[@data-qa="bloko-tag__text"]/text()')
+        item.add_xpath('company_name','//div[@class="vacancy-company-wrapper"]/div[@data-qa="vacancy-company"]//a[@itemprop="hiringOrganization"]/span[@itemprop="name"]/span/text()')
+        item.add_xpath('company_link','//div[@class="vacancy-company-wrapper"]/div[@data-qa="vacancy-company"]//a[@itemprop="hiringOrganization"]/@href')
+
+        yield item.load_item()
 
 # Заголовок
 # URL

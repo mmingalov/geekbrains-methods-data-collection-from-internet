6d13d6280c55dfa6572ae4e0e5c025ae135310d8
mmingalov <mmingalov@gmail.com>
2020-01-25 00:57:34 +1000

    lesson5_practice_second_commit

--- lesson5_practice/gbparse/items.py
+++ lesson5_practice/gbparse/items.py
@@ -9,38 +9,51 @@ import scrapy
 from scrapy.loader.processors import MapCompose, TakeFirst
 
 
-def cleaner_photo(values):
-    if values[:2] == '//':
-        return f'https:{values}'
-    return values
+# def cleaner_photo(values):
+#     if values[:2] == '//':
+#         return f'https:{values}'
+#     return values
+#
+#
+# def cleaner_params(item: str):
+#     result = item.split('">')[-1].split(':')
+#     key = result[0]
+#     value = result[-1].split('</span>')[-1].split('</')[0][:-1]
+#     try:
+#         value = int(value)
+#     except ValueError:
+#         pass
+#
+#     return {key: value}
 
 
-def cleaner_params(item: str):
-    result = item.split('">')[-1].split(':')
-    key = result[0]
-    value = result[-1].split('</span>')[-1].split('</')[0][:-1]
-    try:
-        value = int(value)
-    except ValueError:
-        pass
+# def dict_params(items):
+#     result = {}
+#     for item in items:
+#         result.update(item)
+#     return result
 
-    return {key: value}
+def get_followers_edges(d: dict):
+    result = d["data"]["user"]["edge_followed_by"]["edges"]
+    list=[]
+    for r in result:
+        list.append(r["node"])
+    return list
 
-
-def dict_params(items):
-    result = {}
-    for item in items:
-        result.update(item)
-    return result
-
+def get_following_edges(d: dict):
+    result = d["data"]["user"]["edge_follow"]["edges"]
+    list=[]
+    for r in result:
+        list.append(r["node"])
+    return list
 
 class InstaItem(scrapy.Item):
     # define the fields for your item here like:
     _id = scrapy.Field()
     user_id = scrapy.Field(output_processor = TakeFirst())
     user_name = scrapy.Field(output_processor = TakeFirst())
-    followers = scrapy.Field()
-    following = scrapy.Field()
+    followers = scrapy.Field(input_processor=MapCompose(get_followers_edges))
+    following = scrapy.Field(input_processor=MapCompose(get_following_edges))
     pass
 
 # class GbparseItem(scrapy.Item):
